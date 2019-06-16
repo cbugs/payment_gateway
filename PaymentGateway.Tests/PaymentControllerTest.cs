f7376bae40bfc514b1f8127e1c810710f824ceb7
cbugs <sbrnpjn@gmail.com>
2019-06-16 23:58:29 +0400

    Add Unit Tests

--- PaymentGateway.Tests/PaymentControllerTest.cs
+++ PaymentGateway.Tests/PaymentControllerTest.cs
@@ -1,33 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using PaymentGateway.Api.Controllers;
 using PaymentGateway.Api.Models;
 using PaymentGateway.Api.Abstract;
 using PaymentGateway.Api.Abstract.Factory;
-using PaymentGateway.Data.Models;
-using PaymentGateway.Data.Repository;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace PaymentGateway.Tests
 {
     public class PaymentControllerTest
     {
-
-        public PaymentControllerTest()
-        {
-        }
-
         [Fact]
         public void PaymentMethodFactory_WhenCalled_ReturnsCorrectType()
         {
+            // Arrange
             PaymentModel requestModel_CreditCard = new PaymentModel()
             {
                 Amount = 1000,
                 PaymentMethod = "CreditCard",
-                UserId = Guid.Parse("93adb84b-0be7-4d03-af9d-91a5ff2bbc3q"),
+                UserId = Guid.Parse("e43a794b-9ba5-46d8-8f74-1da699b84092"),
+                Details = "Book",
                 Values = "{\"CardNumber\":\"testc\",\"FullName\":\"testf\",\"Address\":\"testa\",\"ExpiryMonth\":\"testem\",\"ExpiryYear\":\"testey\",\"CVC\":\"testcsv\"}"
             };
             // Act
@@ -36,12 +26,13 @@ namespace PaymentGateway.Tests
             // Assert
             Assert.IsType<CreditCard>(paymentType_CreditCard);
 
-
+            // Arrange
             PaymentModel requestModel_EWallet = new PaymentModel()
             {
                 Amount = 1000,
                 PaymentMethod = "EWallet",
-                UserId = Guid.Parse("93adb84b-0be7-4d03-af9d-91a5ff2bbc3q"),
+                Details = "Laptop",
+                UserId = Guid.Parse("e43a794b-9ba5-46d8-8f74-1da699b84092"),
                 Values = "{\"Username\":\"testc\",\"Password\":\"testf\"}"
             };
             // Act
@@ -49,8 +40,6 @@ namespace PaymentGateway.Tests
 
             // Assert
             Assert.IsType<EWallet>(paymentType_EWallet);
-
         }
-
     }
 }
